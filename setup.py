d33df4ac0129ab73ca08286c3886ea907ff9aa40
Stefano Moia <s.moia@bcbl.eu>
2021-02-09 23:08:14 +0100

    Automatic stuff and real setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ SETUP_REQUIRES = ['setuptools >= 30.3.0']
 SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
 
 if __name__ == "__main__":
-    setup(name='phys2denoise',
+    setup(name='niutils',
           setup_requires=SETUP_REQUIRES,
           version=versioneer.get_version(),
           cmdclass=versioneer.get_cmdclass())
