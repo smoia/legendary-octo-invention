Correct init and add variance weighted average!

niutils/niutils.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-import argparse
 import os
-import sys
 
 import nibabel as nib
 import numpy as np
 
+SUB_LIST = ['001', '002', '003', '004', '007', '008', '009']
+LAST_SES = 10  # 10
+
+LAST_SES += 1
 
 SET_DPI = 300
 FIGSIZE = (18, 10)
@@ -62,6 +64,72 @@ def export_nifti(data, img, fname):
 #############
 # Workflows #
 #############
+
+
+def variance_weighted_average(fname,
+                              fdir='',
+                              exname='',
+                              sub_list=SUB_LIST,
+                              last_ses=LAST_SES):
+    """
+    Compute the variance weighted average of a multi-session study.
+
+    It's supposed that:
+    - all files are in the same folder
+    - `fname` contains placeholders `{sub}` and `{ses}`
+    """
+    # Prepare dictionaries
+    mask = dict.fromkeys(sub_list)
+    data = dict.fromkeys(sub_list)
+    data['avg'] = dict.fromkeys(sub_list)
+    data['var'] = dict.fromkeys(sub_list)
+
+    if fdir:
+        fname = os.path.join(fdir, fname)
+    elif os.path.split(fname)[0]:
+        fdir = os.path.split(fname)[0]
+
+    # Load niftis of all subjects
+    for sub in sub_list:
+        data[sub] = {}
+        mask[sub] = {}
+        for ses in range(1, last_ses):
+            # Load data
+            fname = fname.format(sub=sub, ses=f'{ses:02g}')
+            data[sub][ses], mask[sub][ses], img = load_nifti_get_mask(fname, dim=3)
+
+        # Stack in 4d (axis 3) and mask data (invert nimg mask for masked array)
+        mask[sub]['stack'] = np.stack(mask[sub].values(), axis=3)
+        data[sub]['stack'] = np.ma.array(np.stack(data[sub].values(), axis=3),
+                                         mask=abs(mask[sub]['stack']-1))
+
+        # Compute average & variance of masked voxels across d4
+        data['avg'][sub] = data[sub]['stack'].mean(axis=3)
+        data['var'][sub] = ((data[sub]['stack'] -
+                             data['avg'][sub][:, :, :, np.newaxis])**2).mean(axis=3)
+
+    # Stack subjects in 4d
+    for val in ['avg', 'var']:
+        data[val]['all'] = np.stack(data[val].values(), axis=3)
+
+    # Invert variance & set infinites to zero (if any)
+    invvar = 1 / data['var']['all']
+    invvar[np.isinf(invvar)] = 0
+
+    # Mask group average using invvar
+    data['avg']['all'] = np.ma.array(data['avg']['all'], mask=[invvar == 0])
+
+    # Finally, compute variance weighted average & fill masked entries with 0
+    wavg = np.ma.average(data['avg']['all'], weights=invvar, axis=3).filled(0)
+
+    # Export
+    if not exname and fdir:
+        exname = os.path.split(fdir)[-1]
+    if not exname or exname == '.':
+        exname = 'wavg'
+    else:
+        exname = f'wavg_{exname}'
+    export_nifti(wavg.astype(float), img, exname)
 
 
 def compute_metric(data, atlases, mask, metric='avg'):